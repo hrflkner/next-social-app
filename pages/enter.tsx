7a7d840cd220bae3b6e197a95d36901db7e4f2a0
hrflkner <hunter.wm.faulkner@gmail.com>
2022-07-15 16:05:54 +1000

    ✨ Add Sign in / Sign out
    Add two functional components two sign in / sign out page. Add google
    logo to sign in button and styled container for buttons.

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,12 +1,91 @@
+// Hooks
+import { useContext } from 'react';
+
+// Context
+import { UserContext } from '../lib/context/UserContext';
+
+// Next Components
+import Image from 'next/image';
+
+// Styling
+import styled from 'styled-components';
+
+// Firebase Authentication Service
+import { auth, googleAuthProvider } from '../lib/config/firebaseConfig';
+
+const StyledSignIn = styled.div`
+    button {
+        background-color: white;
+        color: var(--color-text);
+    }
+
+    .img-container {
+        margin-right: 10px;
+    }
+
+    p {
+        display: inline-block;
+    }
+`;
+
 type Props = {};
 
 function EnterPage({}: Props) {
+    const { user, username } = useContext(UserContext);
+
     return (
         <main>
-            <h1>Enter</h1>
-            <p>Sign Up Here:</p>
+            {user ? (
+                !username ? (
+                    <UsernameForm />
+                ) : (
+                    <SignOutButton />
+                )
+            ) : (
+                <div>
+                    <p>Sign Up Here:</p>
+                    <SignInButton />
+                </div>
+            )}
         </main>
     );
+}
+
+function SignInButton() {
+    const signInWithGoogle = async () => {
+        try {
+            auth.signInWithPopup(googleAuthProvider);
+        } catch (error) {
+            console.error('Failed to login with Google:', error);
+        }
+    };
+
+    return (
+        <StyledSignIn>
+            <button className="btn-google" onClick={signInWithGoogle}>
+                <div className="img-container">
+                    <Image
+                        width="20px"
+                        height="20px"
+                        src={'/googlelogo.png'}
+                        alt="Google Logo"
+                    />
+                </div>
+                <p>Sign in with Google</p>
+            </button>
+            <button onClick={() => auth.signInAnonymously()}>
+                Sign in Anonymously
+            </button>
+        </StyledSignIn>
+    );
+}
+
+function SignOutButton() {
+    return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+    return null;
 }
 
 export default EnterPage;
