26c3cb052974ea3836abfc9500037dfd8b283112
hrflkner <hunter.wm.faulkner@gmail.com>
2022-07-15 15:23:43 +1000

    Configure Fetch Image URL
    Next.js requires configurations for image fetch links. Link added
    fetches google user's profile picture for display on site after logging
    in with Google Auth (Firebase).

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+        minimumCacheTTL: 1500000,
+    },
+    swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
