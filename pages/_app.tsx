6af6a22f4f705bc6a0acbcc26884d4a05f59ceb7
hrflkner <hunter.wm.faulkner@gmail.com>
2022-07-15 16:09:37 +1000

    ✨ Import Context Provider and Toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,30 @@
+// Global Styling
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
 
+// Next Components
+import type { AppProps } from 'next/app';
 import { GetServerSideProps } from 'next';
 
+// Components
+import NavBar from '../components/shared/Navbar';
+
+// Context Provider
+import UserContextProvider from '../lib/context/UserContext';
+
+// Toaster Popups Provider
+// is Invisible unless action from children is called
+import { Toaster } from 'react-hot-toast';
+
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <UserContextProvider>
+                <NavBar />
+                <Component {...pageProps} />
+                <Toaster />
+            </UserContextProvider>
+        </>
+    );
 }
 
 export default MyApp;
